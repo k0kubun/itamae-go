4c9e48ad5bb53d80bb85e321f972348f4319e8ab
Takashi Kokubun <takashikkbn@gmail.com>
2016-07-23 01:09:59 +0900

    Parse itamae local options

--- command/local.go
+++ command/local.go
@@ -1,26 +1,58 @@
 package command
 
 import (
+	"flag"
 	"strings"
+
+	"github.com/k0kubun/pp"
 )
 
 type LocalCommand struct {
 	Meta
+
+	dryRun   bool
+	nodeJson string
+	logLevel string
+	recipes  []string
 }
 
 func (c *LocalCommand) Run(args []string) int {
-	// Write your code here
+	flags := flag.NewFlagSet("itamae-go", flag.ContinueOnError)
 
+	flags.BoolVar(&c.dryRun, "n", false, "Dry run")
+	flags.BoolVar(&c.dryRun, "dry-run", false, "Dry run")
+	flags.StringVar(&c.nodeJson, "j", "", "Node JSON")
+	flags.StringVar(&c.nodeJson, "node-json", "", "Node JSON")
+	flags.StringVar(&c.logLevel, "l", "", "Log level")
+	flags.StringVar(&c.logLevel, "log-level", "", "Log level")
+
+	if err := flags.Parse(args); err != nil {
+		return 1
+	}
+	for 0 < flags.NArg() {
+		c.recipes = append(c.recipes, flags.Arg(0))
+		flags.Parse(flags.Args()[1:])
+	}
+
+	pp.Println(c)
 	return 0
 }
 
 func (c *LocalCommand) Synopsis() string {
-	return ""
+	return "Run itamae locally"
 }
 
 func (c *LocalCommand) Help() string {
 	helpText := `
+Usage:
+  itamae local RECIPE [RECIPE...]
 
+Options:
+  -j, [--node-json=NODE_JSON]
+  -n, [--dry-run]
+  -l, [--log-level=LOG_LEVEL]
+
+Run Itamae locally
 `
 	return strings.TrimSpace(helpText)
 }
