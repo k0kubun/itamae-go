093d4b9eb5df354e048e30c7dbcd544249ad6d78
Takashi Kokubun <takashikkbn@gmail.com>
2016-07-23 01:14:27 +0900

    Vendoring help function

--- cli.go
+++ cli.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"log"
 	"os"
+	"sort"
+	"strings"
 
 	"github.com/k0kubun/itamae-go/command"
 	"github.com/mitchellh/cli"
@@ -43,7 +47,7 @@ func RunCustom(args []string, commands map[string]cli.CommandFactory) int {
 		Args:       args,
 		Commands:   commands,
 		Version:    Version,
-		HelpFunc:   cli.BasicHelpFunc(Name),
+		HelpFunc:   BasicHelpFunc(Name),
 		HelpWriter: os.Stdout,
 	}
 
@@ -53,4 +57,48 @@ func RunCustom(args []string, commands map[string]cli.CommandFactory) int {
 	}
 
 	return exitCode
+}
+
+func BasicHelpFunc(app string) cli.HelpFunc {
+	return func(commands map[string]cli.CommandFactory) string {
+		var buf bytes.Buffer
+		buf.WriteString(fmt.Sprintf(
+			"usage: %s [--version] [--help] <command> [<args>]\n\n",
+			app))
+		buf.WriteString("Available commands are:\n")
+
+		// Get the list of keys so we can sort them, and also get the maximum
+		// key length so they can be aligned properly.
+		keys := make([]string, 0, len(commands))
+		maxKeyLen := 0
+		for key, _ := range commands {
+			if len(key) > maxKeyLen {
+				maxKeyLen = len(key)
+			}
+
+			keys = append(keys, key)
+		}
+		sort.Strings(keys)
+
+		for _, key := range keys {
+			commandFunc, ok := commands[key]
+			if !ok {
+				// This should never happen since we JUST built the list of
+				// keys.
+				panic("command not found: " + key)
+			}
+
+			command, err := commandFunc()
+			if err != nil {
+				log.Printf("[ERR] cli: Command '%s' failed to load: %s",
+					key, err)
+				continue
+			}
+
+			key = fmt.Sprintf("%s%s", key, strings.Repeat(" ", maxKeyLen-len(key)))
+			buf.WriteString(fmt.Sprintf("    %s    %s\n", key, command.Synopsis()))
+		}
+
+		return buf.String()
+	}
 }
