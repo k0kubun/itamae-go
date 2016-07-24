24dd3dc4bd9973ee9f94bcd89804f0541e59119b
Takashi Kokubun <takashikkbn@gmail.com>
2016-07-25 00:33:19 +0900

    Parse execute resource

--- recipe/dsl/execute.go
+++ recipe/dsl/execute.go
@@ -1,12 +1,32 @@
 package dsl
 
 import (
-	"fmt"
-
+	"github.com/k0kubun/itamae-go/recipe/resource"
+	"github.com/k0kubun/itamae-go/recipe/resource/utils"
 	"github.com/mitchellh/go-mruby"
 )
 
 func Execute(mrb *mruby.Mrb, self *mruby.MrbValue) (mruby.Value, mruby.Value) {
-	fmt.Println("execute resource (stubbed)")
+	args := mrb.GetArgs()
+	utils.AssertType("command", args[0], mruby.TypeString)
+
+	exec := resource.Execute{
+		Command: args[0].String(),
+	}
+
+	parser := utils.NewAttributeParser(mrb)
+	parser.SetDefaultString("action", "run")
+	if len(args) > 1 {
+		utils.AssertType("block", args[1], mruby.TypeProc)
+		parser.Parse(args[1])
+	}
+
+	exec.Action = parser.GetArray("action")
+	exec.User = parser.GetString("user")
+	exec.Cwd = parser.GetString("cwd")
+	exec.OnlyIf = parser.GetString("only_if")
+	exec.NotIf = parser.GetString("not_if")
+
+	resource.Register(&exec)
 	return mruby.Nil, nil
 }
