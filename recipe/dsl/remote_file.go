61c8cb775e2c5129c0aaf20b7d6c4d574e4107f0
Takashi Kokubun <takashikkbn@gmail.com>
2016-07-25 00:52:12 +0900

    Parse remote_file resource

--- recipe/dsl/remote_file.go
+++ recipe/dsl/remote_file.go
@@ -1,12 +1,37 @@
 package dsl
 
 import (
-	"fmt"
-
+	"github.com/k0kubun/itamae-go/recipe/resource"
+	"github.com/k0kubun/itamae-go/recipe/resource/utils"
 	"github.com/mitchellh/go-mruby"
 )
 
 func RemoteFile(mrb *mruby.Mrb, self *mruby.MrbValue) (mruby.Value, mruby.Value) {
-	fmt.Println("remote_file resource (stubbed)")
+	args := mrb.GetArgs()
+	utils.AssertType("path", args[0], mruby.TypeString)
+
+	file := resource.RemoteFile{
+		Path: args[0].String(),
+	}
+
+	parser := utils.NewAttributeParser(mrb)
+	parser.SetDefaultString("action", "create")
+	if len(args) > 1 {
+		utils.AssertType("block", args[1], mruby.TypeProc)
+		parser.Parse(args[1])
+	}
+
+	file.Action = parser.GetArray("action")
+	file.User = parser.GetString("user")
+	file.Cwd = parser.GetString("cwd")
+	file.OnlyIf = parser.GetString("only_if")
+	file.NotIf = parser.GetString("not_if")
+	file.Content = parser.GetString("content")
+	file.Mode = parser.GetString("mode")
+	file.Owner = parser.GetString("owner")
+	file.Group = parser.GetString("group")
+	file.Source = parser.GetString("source")
+
+	resource.Register(&file)
 	return mruby.Nil, nil
 }
